889219b1618082b9ca8f7f7b6d69ec75a5759b14
Jiri Horner <laeqten@gmail.com>
2017-03-17 13:42:16 +0100

    added ros-devel

---
+++ ros-devel/ros-devel.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# 	--device /dev/snd \
+#	--device /dev/dri \
+#	--device /dev/usb \
+#	--device /dev/bus/usb \
+
+# xauth -f docker.Xauthority.hostname-specific generate :0 . untrusted
+
+
+docker run -it \
+	--user $(id -u) \
+	-v /etc/group:/etc/group:ro \
+	-v /etc/passwd:/etc/passwd:ro \
+	-v /etc/shadow:/etc/shadow:ro \
+	-v /etc/sudoers.d:/etc/sudoers.d:ro \
+	-v /etc/localtime:/etc/localtime:ro \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e DISPLAY=$DISPLAY \
+	-e XAUTHORITY=/home/$USER/.Xauthority \
+	-v /home/$USER/.ros_home:/home/$USER \
+	-v /home/$USER/robocup:/home/$USER/robocup \
+	-v /home/$USER/ros:/home/$USER/ros \
+	-v /home/$USER/.zshrc:/home/$USER/.zshrc:ro \
+	-v /home/$USER/.gitconfig:/home/$USER/.gitconfig:ro \
+	--workdir=/home/$USER/ros/workspace \
+	--device /dev/video0 \
+	--name ros-devel \
+	-h ros-devel \
+	--rm=true \
+	hrnr/ros-devel
+
