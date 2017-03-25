7e1e805cb6bad548fb97a1bd1706035930dda643
Jiri Horner <laeqten@gmail.com>
2017-03-25 12:47:38 +0100

    add ros-ycmd image

---
+++ ros-ycmd/ros-ycmd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# run ycmd on 49549
+
+docker run -d \
+	-p 127.0.0.1:49549:49549 \
+	-v /home/$USER/.config/ycmd:/etc/ycmd:ro \
+	-v /home/$USER:/home/$USER:ro \
+	--name ros-ycmd \
+	hrnr/ros-ycmd
+
