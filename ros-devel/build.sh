4b765d932a9dcb2ce9e5f51750c1d63a8c74aadf
Jiri Horner <laeqten@gmail.com>
2017-04-13 11:30:53 +0200

    parametrize build with env vars

--- ros-devel/build.sh
+++ ros-devel/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 ros_latest=$(cat ros-latest)
-ros_version=${1:-$ros_latest}
+ros_version=${ROS_VERSION:-$ros_latest}
 
 cp Dockerfile Dockerfile.bkp
 sed -e "s/kinetic/${ros_version}/" Dockerfile.bkp > Dockerfile
 
-docker build -t hrnr/ros-devel:${ros_version} --build-arg ros_version=${ros_version} .
+IMAGE_NAME=${IMAGE_NAME:-hrnr/ros-devel:${ros_version}}
+
+docker build -t $IMAGE_NAME --build-arg ros_version=${ros_version} .
 
 mv Dockerfile.bkp Dockerfile
