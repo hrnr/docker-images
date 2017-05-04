c62f72159d69bd92d85229f205e7b8897a183b52
Jiri Horner <laeqten@gmail.com>
2017-05-04 11:51:07 +0200

    mount Xauthority into container
    * to support connecting to forwarded X11 over ssh

--- ros-robot-p/ros-robot-p.sh
+++ ros-robot-p/ros-robot-p.sh
@@ -29,9 +29,10 @@ docker run -it \
 	-v /etc/shadow:/etc/shadow:ro \
 	-v /etc/sudoers.d:/etc/sudoers.d:ro \
 	-v /etc/localtime:/etc/localtime:ro \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=$DISPLAY \
 	-e XAUTHORITY=$HOME/.Xauthority \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v $HOME/.Xauthority:$HOME/.Xauthority:ro \
 	-v $HOME/.ros_home:$HOME \
 	-v $HOME/.gitconfig:$HOME/.gitconfig:ro \
 	-v $ros_workspace:$ros_workspace \
