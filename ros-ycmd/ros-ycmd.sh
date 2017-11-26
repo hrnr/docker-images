6eb55b7ad7823d459826c4bc1f5c57e20949ad9a
Jiri Horner <laeqten@gmail.com>
2017-11-26 19:30:31 +0100

    rework ros-ycmd
    * use ycmd from ros-devel image
    * get rid of Dockerfile, it is just start script

--- ros-ycmd/ros-ycmd.sh
+++ ros-ycmd/ros-ycmd.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-ros_latest=$(cat ../ros-devel/ros-latest)
+. ../ros-common/ros-common.sh
+
+# start ycmd daemon -- port 4954, autokill in 12h
+ycmd_command="ycmd --host=0.0.0.0 --port=49549 --options_file=/etc/ycmd/default_settings.json --idle_suicide_seconds=43200 --log=debug"
 
 # run ycmd on 49549
 docker run -d \
+	--expose=49549 \
+	--user=daemon \
 	-p 127.0.0.1:49549:49549 \
 	-v /home/$USER/.config/ycmd:/etc/ycmd:ro \
 	-v /home/$USER:/home/$USER:ro \
 	--name ros-ycmd \
-	hrnr/ros-ycmd
-
+	--entrypoint="" \
+	hrnr/ros-devel:${ros_distro} \
+	$ycmd_command
